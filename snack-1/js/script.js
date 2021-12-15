c4383c47adbd379f68a8f5735a1b370e7bfbe520
Cezar <cezarlantosgmail.com>
2021-12-15 15:21:17 +0100

    snack-1 finito

---
+++ snack-1/js/script.js
@@ -0,0 +1,9 @@
+let sum = 0;
+let n;
+
+for (let i = 0; i < 10; i++) {
+    n = parseInt(prompt('Inserisci un numero casuale :) !!'));
+    sum += n;
+}
+
+console.log('La somma totale dei numeri inseriti è di:', sum);
