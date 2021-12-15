697c2e9c5f4e5c5dbef48dc14617d9200f32e87e
Cezar <cezarlantosgmail.com>
2021-12-15 18:03:15 +0100

    snack-4

---
+++ snack-3/js/script.js
@@ -0,0 +1,21 @@
+const outPut = document.querySelector('.output');
+
+let x = 2;
+let n; 
+
+for(let i = 0; i < 1000; i++) {
+    potenza = x ** n;
+}
+
+
+
+
+// let x = 2;
+// let y = 1000;
+// while (x <= y) {
+//     i = Math.pow(x, 3);
+//     console.log(i);
+//     i++;
+// }
+
+outPut.innerHTML = `${i}`;
---
+++ snack-4/js/script.js
@@ -0,0 +1,17 @@
+let userStringNumber;
+
+do {
+    userStringNumber = prompt('Inserisci un numero di almeno 4 cifre.');
+} while(userStringNumber.length < 4 || userStringNumber.length > 4);
+
+// let sum = parseInt(userStringNumber[0]) + parseInt(userStringNumber[1]) +  parseInt(userStringNumber[2]) +  parseInt(userStringNumber[3]);
+
+let sum = 0;
+
+for(let i = 0; i < userStringNumber.length; i++) {
+    sum += parseInt(userStringNumber[i]); 
+}
+
+console.log(sum);
+
+
